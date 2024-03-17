1ce1576a25113f12cc714f687e23d8a0b5857557
Connor <cguy33333@gmail.com>
2024-03-16 23:18:11 -0700

    a lot of small things, plus a ton of functionality for the party system as well as other pages in the website

--- assets/script.js
+++ assets/script.js
@@ -5,11 +5,28 @@ number = 0;
 function ennumerate() {
         number++;
         console.log(number);
+        localStorage.setItem("clicks", JSON.stringify(number))
 
 }
+// The update function itself only works when we are on the click page
+if (document.URL.includes("click.html")) {
+    // Calls our update id in the HTML and updates it every 10 milliseconds
+    var t = setInterval(function() {
+        document.getElementById("update").innerHTML = number;
+    }, 10);
+}
+//------------------------- PARTY FUNCTIONS -------------------------
+var select = document.getElementById("selector");
 
-var t = setInterval(function() {
-    document.getElementById("update").innerHTML = number;
-}, 10);
+function selectBox(value) {
+    // triple equals just doesnt type convert so its stricter
+    if (select.style.display === "none") {
+        select.style.display = "block";
+    }
+    else {
+        select.style.display = "none";
+    }
+    document.getElementById("data").innerText=document.getElementById(JSON.stringify(value)).innerText
+}
 
 console.log(number);

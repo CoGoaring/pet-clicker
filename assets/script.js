2a0b1b41d710801b5e1472e289413251d9ad916a
Connor <cguy33333@gmail.com>
2024-03-20 14:29:06 -0700

    Heavy changes to structure, functionality added to home, stables, party, and some extra to clicks. Party currently half broken.

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,10 @@
+// This is from google, may use this for testing as a psuedo-database
+//let db;
+
 const petID = [0,1,2,3,4,5,6,7,8,9,10];
+
+// I will need to create a pet object eventually, and have an a array of them like this.
+myPet = [];
 
 number = 0;
 
@@ -7,12 +13,34 @@ function ennumerate() {
         console.log(number);
         localStorage.setItem("clicks", JSON.stringify(number))
 
+        // 50% chance to recieve a pet, not how I want this system to work but functional
+        if(getRandomInt(2) == 1) {
+            myPet.push(getRandomInt(petID.length+1))
+            localStorage.setItem("myPet", JSON.stringify(myPet))
+        }
+        // var petVal = JSON.parse(localStorage.getItem('myPet'));
+
+         console.log(myPet);
+         
 }
+var petVal = JSON.parse(localStorage.getItem('myPet'));
+
+// Random number starting at 0 rounded to nearest whole float
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
 // The update function itself only works when we are on the click page
 if (document.URL.includes("click.html")) {
     // Calls our update id in the HTML and updates it every 10 milliseconds
     var t = setInterval(function() {
         document.getElementById("update").innerHTML = number;
+    }, 10);
+}
+if (document.URL.includes("stables.html")) {
+    // Calls our update id in the HTML and updates it every 10 milliseconds
+    var t = setInterval(function() {
+        document.getElementById("updatePets").innerHTML = petVal;
     }, 10);
 }
 //------------------------- PARTY FUNCTIONS -------------------------

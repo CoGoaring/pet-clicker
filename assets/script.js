cfe238d3c3fecee86ae30270e676fbaa64cb40f8
Connor <cguy33333@gmail.com>
2024-04-06 21:57:06 -0700

    refactored the CSS again, with the root and all of the base properties in one base css file, also added a font but will likley replace it with a better one later.

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,9 @@
 // This is from google, may use this for testing as a psuedo-database
 //let db;
 
+// TODO: Can I use some form of reload cache? for certain things only but might reduce network lag
+// This is API specific but potentially useful, mostly just keep memory usage as low as possible
+
 const petID = [0,1,2,3,4,5,6,7,8,9,10];
 // your img for both egg and pokemon has to equal your id so there arent 10 million if statements
 const pet = {
@@ -20,12 +23,12 @@ let number=0;
 function ennumerate() {
         number++;
         console.log(number);
-        localStorage.setItem("clicks", JSON.stringify(number))
+        localStorage.setItem("clicks", JSON.stringify(number));
 
         // 50% chance to recieve a pet, not how I want this system to work but functional
         if(getRandomInt(2) == 1) {
-            myPet.push(getRandomInt(petID.length+1))
-            localStorage.setItem("myPet", JSON.stringify(myPet))
+            myPet.push(getRandomInt(petID.length+1));
+            localStorage.setItem("myPet", JSON.stringify(myPet));
         }
         console.log(myPet);
          
